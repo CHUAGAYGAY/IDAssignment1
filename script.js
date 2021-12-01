8dca83eb8b9ce291eb052b7fded21a73a9db2575
CHUAGAYGAY <s10227853@connect.np.edu.sg>
2021-12-01 18:10:33 +0800

    Edited Footer Section

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
 
 
     // Typing Animation
-    let types = new Typed(".type1", {
+    let type = new Typed(".type1", {
         strings: ["Chua Cheng Xuan", "蔡承轩"],
         typeSpeed: 70,
         backSpeed: 50,
@@ -52,7 +52,7 @@ $(document).ready(function(){
         
     });
 
-    let type = new Typed(".type2", {
+    let types = new Typed(".type2", {
         strings: ["Ngee Ann Polytechnic", "The School of ICT", "Singapore"],
         typeSpeed: 70,
         backSpeed: 20,
@@ -65,3 +65,59 @@ $(document).ready(function(){
         $('html, body').animate({scrollTop: 0}, "fast");
     });
 });
+
+// Contact Form Input Data
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error");
+
+// Contact Form Input Validation 
+function validateForm() {
+
+    clearMessages();
+    let errorFlag = false;
+
+    if(nameInput.value.length < 1){
+        errorNodes[0].innerText = "Please Input Your Name";
+        nameInput.classList.add("error-border");
+        errorFlag = true;
+    }
+
+    if(!emailIsValid(email.value)){
+        errorNodes[1].innerText = "Please Enter A Valid Email Address";
+        email.classList.add("error-border");
+        errorFlag = true;
+    }
+
+    if(message.value.length < 1){
+        errorNodes[2].innerText = "Please Enter Your Message";
+        message.classList.add("error-border");
+        errorFlag = true;
+    }
+
+    if (!errorFlag){
+        success.innerText = "Message Sent Successfully, Thank You! :)";
+    }
+}
+
+
+// Clears Error / Success Messages
+function clearMessages(){
+    
+    for(let i = 0; i < errorNodes.length; i++){
+        errorNodes[i].innerText = "";
+    }
+
+    success.innerText = "";
+    nameInput.classList.remove("error-border");
+    email.classList.remove("error-border");
+    message.classList.remove("error-border");
+}
+
+// To Check Whether Email Is Valid
+function emailIsValid(email){
+    let pattern = /\S+@\S+\.\S+/;
+    return pattern.test(email);
+}
